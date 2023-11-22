5f7545451a9a864c9bef3d724b462c22c3846b6c
AlessandroNJ <alessandronj00@gmail.com>
2023-11-22 12:20:36 -0300

    teste django

---
+++ hello/views.py
@@ -0,0 +1,20 @@
+import re
+from django.utils.timezone import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+    print(request.build_absolute_uri()) #optional
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
+print("http://127.0.0.1:8000/hello/name")
